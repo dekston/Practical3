e43cc1ce0e2f1455f7deb45b8e1748b2cfa9780c
wai lun <dekstonfung@gmail.com>
2019-11-11 11:59:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practical3/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.practical3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
+    override fun onItemClick(parent: AdapterView<*>?,
+                             view: View?,
+                             position: Int,
+                             id: Long) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        spinnerAge.setOnItemClickListener(this)
+        buttonCalculate.setOnClickListener{
+            calculatePremium()
+        }
+    }
+
+    private fun calculatePremium() {
+        val position=spinnerAge.selectedItemPosition
+        val gender= radioGroupGender.checkedRadioButtonId
+        var premium: Int
+
+        if(position==1){
+            premium=60
+        }
+        if(gender==R.id.radioButtonMale){
+            
+        }
+        if(gender==R.id.radioButtonFemale){
+
+        }
+        if(checkBoxSmoker.isChecked){
+
+        }
+
+    }
+
+}
